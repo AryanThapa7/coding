a44492200d6f9260cfcc3e8ba4d53bf3c5c43f5d
Aryanthapa7 <aryanarthapa@gmail.com>
2026-03-03 11:53:39 +0545

    intro to js

---
+++ 01.js
@@ -0,0 +1,7 @@
+var a = 10;
+let b = "deerwalk";
+let college = "deerwalk";
+let collegeName = "deerwalk"; //camel casing
+let college_name = "deerwalk"; // snake casing 
+let CollegeName = "deerwalk"; //
+const id = 2; 
