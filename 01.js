b4f516d04885c80e9b6507b785cb1c59952fcec6
Aryanthapa7 <aryanarthapa@gmail.com>
2026-03-09 12:08:01 +0545

    basic js username

--- 01.js
+++ 01.js
@@ -1,39 +1,7 @@
-let age = 30 ;
-let temperature = 32.14123; 
-let amount = 100000;
-// console.log(age);
-// console.log(temperature);
-
-console.log(temperature.toFixed(2)); // 32.14
-console.log(amount.toLocaleString()); // 100,000
-console.log(Math.PI); // 3.141592653589793
-console.log(Math.floor(temperature)); // 32
-console.log(Math.ceil(temperature)); // 33
-console.log(Math.random()); // 0.123456789 (random number between 0 and 1)
-console.log(Math.floor(Math.random()*10  )); // 1.23456789 (random number between 0 and 10)
-
-let min = 50;
-let max = 100;
-let random = Math.floor(Math.random() * (max-min)+1)+min; // random number between 50 and 100  
-console.log(random);
-
-//for ludo game we need random number between 1 and 6
-const MIN = 1 ;
-const MAX = 6 ;
-
-let randomNumber = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN; // random number between 1 and 6
-console.log(randomNumber);
-
-const Min = 100000 ; 
-const Max = 999999 ;
-
-let otp = Math.floor(Math.random() * (Max - Min + 1)) + Min; // random number between 100000 and 999999
-console.log(otp);
-
-console.log(5 ** 2); // 25
-
-// == loose equality operator
-// === strict equality operator
-
-console.log(5 == '5'); // true , checks only value not data type
-console.log(5 === '5'); // false , checks value and data type
+let username = "John Doe";
+console.log(typeof username);
+console.log(username[1]);
+console.log(username.charAt(1));
+console.log(username.length);
+console.log(username.toUpperCase());
+console.log(username.toLowerCase());
