c092e5e05a4480f3b80170dc9ef5fa8f6c8af475
Aryanthapa7 <aryanarthapa@gmail.com>
2026-03-09 12:29:42 +0545

    js basics

--- 01.js
+++ 01.js
@@ -10,10 +10,19 @@ console.log(username.lastIndexOf("z"));
 console.log(username.includes("Doe"));
 console.log(username.includes("."));
 console.log(username.slice(0, 4)); // because the end index is exclusive and the start index is inclusive and the end index is 4 so it will return the characters from index 0 to index 3
-*/
+
 
 let email = "susan.shakya@deerwalk.edu.np";
 
 let username = email.slice(0, email.indexOf("."));
 
 console.log(username);
+*/
+let username = "John Doe";
+let email = "john.doe@example.com";
+// console.log(email.split(".")[0]); // it will split the email string into an array of three elements and return the first element which is the username part of the email address
+
+let fname = " John ";
+let lname = " Doe ";
+console.log("My first name is " + fname + " and my last name is " + lname); // it will remove the leading and trailing whitespace from the string
+console.log(`My first name is ${fname} and my last name is ${lname}`); // it will remove the leading and trailing whitespace from the string
