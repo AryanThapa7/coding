b837ddd1e22734fe6e37509010282717546d28cb
Aryanthapa7 <aryanarthapa@gmail.com>
2026-03-10 12:18:39 +0545

    objects in js

--- 01.js
+++ 01.js
@@ -1,8 +1,21 @@
 // arrays in js 
 
-let numbers = [1, 2, 3, 4, " five " , true ];
+// let numbers = [1, 2, 3, 4, " five " , true ];
 // numbers.pop();
 // numbers.shift();
 // numbers.unshift(-1);
 
-console.log(numbers);
+
+// console.log(numbers);
+
+// objects and arrays in js
+
+let a = [ "Ram ", 12 , "ktm"];
+let user = {
+    name: "Ram",
+    age: 12,
+    city: "ktm"
+};
+console.log(user.name);
+console.log(user.age);
+console.log(user.city);
