10e0c2954db1c6b07bb79744a6c4f916126cfa5f
Aryanthapa7 <aryanarthapa@gmail.com>
2026-03-10 12:22:04 +0545

    switch case in js

--- 01.js
+++ 01.js
@@ -19,3 +19,15 @@ let user = {
 console.log(user.name);
 console.log(user.age);
 console.log(user.city);
+
+// for switch case in js
+
+let age = 18 ;
+
+if (age < 18) {
+    console.log("You are a minor");
+} else if (age >= 18 && age < 60) {
+    console.log("You are an adult");
+} else {
+    console.log("You are a senior citizen");
+}
