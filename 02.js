428417dc735fb346ce0facb25da8f4807a685277
Aryanthapa7 <aryanarthapa@gmail.com>
2026-03-16 12:31:59 +0545

    simple heading change through js

--- 02.js
+++ 02.js
@@ -1,13 +1,3 @@
-/* const heading = document.getElementById('heading');
-console.log(heading);
-const ptext = document.getElementsByClassName('text');
-console.log(ptext); */
+const heading = document.getElementById("heading");
+heading.innerText = "Hello, World!";
 
-const heading = document.querySelector('#heading');
-console.log(heading);
-
-const ptext = document.querySelector('.text');
-console.log(ptext);
-
-const ptxts = document.querySelectorAll('.text');
-console.log(ptxts);
