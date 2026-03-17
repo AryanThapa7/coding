00394906577487be54f27766fdadd50c0330937e
Aryanthapa7 <aryanarthapa@gmail.com>
2026-03-17 12:02:41 +0545

    new changes in js

--- 02.js
+++ 02.js
@@ -1,3 +1,28 @@
-const heading = document.getElementById("heading");
-heading.innerText = "Hello, World!";
+/* const text = document.getElementById("text");
 
+text.style.color = "red";
+text.style.fontSize = "24px";
+text.style.fontWeight = "bold";
+text.style.textDecoration = "underline";
+text.style.textAlign = "center";
+text.style.backgroundColor = "yellow";
+text.style.padding = "10px";
+text.style.border = "2px solid black";
+text.style.borderRadius = "10px";
+text.style.width = "300px";
+text.style.margin = "20px auto"; */
+
+const text = document.getElementById("text");
+const glink = document.getElementById("link");
+glink.setAttribute("href", "https://www.google.com");
+text.style.color = "blue";
+text.style.fontSize = "20px";
+text.style.fontWeight = "bold";
+text.style.textDecoration = "none";
+text.style.textAlign = "center";
+text.style.backgroundColor = "lightgray";
+text.style.padding = "10px";
+text.style.border = "2px solid black";
+text.style.borderRadius = "10px";
+text.style.width = "300px";
+text.style.margin = "20px auto";
