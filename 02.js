c0b07018adf30710e1dc26a38ad9efcb6585204a
Aryanthapa7 <aryanarthapa@gmail.com>
2026-03-17 12:26:48 +0545

    simple hover in and out image

--- 02.js
+++ 02.js
@@ -1,28 +1,11 @@
-/* const text = document.getElementById("text");
+const img = document.getElementById("myImage");
 
-text.style.color = "red";
-text.style.fontSize = "24px";
-text.style.fontWeight = "bold";
-text.style.textDecoration = "underline";
-text.style.textAlign = "center";
-text.style.backgroundColor = "yellow";
-text.style.padding = "10px";
-text.style.border = "2px solid black";
-text.style.borderRadius = "10px";
-text.style.width = "300px";
-text.style.margin = "20px auto"; */
 
-const text = document.getElementById("text");
-const glink = document.getElementById("link");
-glink.setAttribute("href", "https://www.google.com");
-text.style.color = "blue";
-text.style.fontSize = "20px";
-text.style.fontWeight = "bold";
-text.style.textDecoration = "none";
-text.style.textAlign = "center";
-text.style.backgroundColor = "lightgray";
-text.style.padding = "10px";
-text.style.border = "2px solid black";
-text.style.borderRadius = "10px";
-text.style.width = "300px";
-text.style.margin = "20px auto";
+img.addEventListener("mouseenter", () => {
+    img.src = "Unknown.jpeg";
+});
+
+
+img.addEventListener("mouseleave", () => {
+    img.src = "DWITCollege.jpeg";
+});
